8444a0c8dea4cac22ddf2cf4eeb1d3e4cc385938
Michael Stromer <stroms38@gmail.com>
2023-08-21 16:12:00 -0400

    API Auth updates

---
+++ packages/functions/src/list.ts
@@ -0,0 +1,25 @@
+import { Table } from "sst/node/table";
+import handler from "@spark/core/handler";
+import dynamoDb from "@spark/core/dynamodb";
+
+export const main = handler(async (event) => {
+  const params = {
+    TableName: Table.Notes.tableName,
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId'
+    //   partition key
+    KeyConditionExpression: "userId = :userId",
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be the id of the author
+    ExpressionAttributeValues: {
+      ":userId": {
+        "S": event.requestContext.authorizer.iam.cognitoIdentity.identityId,
+      }
+    },
+  };
+
+  const result = await dynamoDb.query(params);
+
+  // Return the matching list of items in response body
+  return result.Items;
+});
